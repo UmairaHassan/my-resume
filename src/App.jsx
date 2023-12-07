567a556391e2ec166474024ced03a04b36b0925a
Umaira Hassan <152908847+UmairaHassan@users.noreply.github.com>
2023-12-07 16:22:39 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import './style.css'
+
+
+function HomePage() {
+  return (
+   
+   <body>
+   <div id="header">
+   <h1 id="name">Umaira Hassan</h1>
+            
+        </div>
+        <div class="left">
+        </div>
+        <div class="right">
+               <p>
+               <h3>Basic Information</h3>
+
+               <ul>
+                   <li>Email Address: umaira_abrar98@hotmail.com</li>
+                   <li>Contact Number: 123-456-789</li>
+                   <li>Address: x-y-z</li></ul>
+               </p>
+               <h3>Professional Experience</h3>
+               <h4 id="company-name">Wingify Software Pvt Ltd</h4>
+               <p id="job-title"><strong>Support Engineer (Technical Support)</strong></p>
+               <p id="job-responsibilities">Job Responsbilities</p>
+               <p>
+               <ul>
+                   <li>Answer customer queries over email / ticketing system</li>
+                   <li>Interact with our engineering team to get software issues and bugs resolved</li>
+                   <li>Occasionally interact with customers over Skype or telephone</li>
+                   <li>Contribute ideas to the team on how customers can be delighted</li></ul>
+               </p>
+               <h3>Educational Qualifications</h3>
+               <table>
+                   <tr id="heading">
+                       <td>Qualification</td>
+                       <td>Board</td>
+                       <td>Percentage / Grades</td>
+                       <td>Year</td>
+                   </tr>
+                   <tr>
+                       <td>Matriculation</td>
+                       <td>Lahore Board</td>
+                       <td>75.57%</td>
+                       <td>2004</td>
+                   </tr>
+                   <tr>
+                       <td>F.A (Science Stream)</td>
+                       <td>Lahore Board</td>
+                       <td>72.40%</td>
+                       <td>2006</td>
+                   </tr>
+                   <tr>
+                       <td>Bachelor in Arts</td>
+                       <td>University Of The Punjab , Pakistan</td>
+                       <td>A Grade</td>
+                       <td>2008</td>
+                   </tr>
+                  
+               </table>
+               <h3>Independent Courses</h3>
+               <p/>
+               <ul>
+                   <li>
+                   <span id="course-name">HTML & CSS for Beginners – Web Fundamentals</span> – NFTP</li>
+                  
+                   <li>
+                   <span id="course-name">JavaScript – Programming Basics, JS Apps </span> - Nexus Skill</li>
+                   <li>
+                   <span id="course-name">Graphic Desiging </span> - Model Town Ladies Club, Lahore</li>
+                   <li>
+                   <span id="course-name">Introduction To Computer Science </span> - Model Town Ladies Club, Lahore.</li>
+               </ul>
+              
+              
+               <h3>Personal Information:</h3>
+               <p>               <ul>
+                
+              <li/>
+                   A young, determined hard and smart working person. I believe in task based roles and complete ownership of work.
+                   <li>
+                   <span id="course-name">Languages Known:</span>English, Urdu, and Punjabi </li>
+                   <li>
+                   <span id="course-name">Hobbies:</span>I love reading Copmuter Programing  and IT related books / magazines, playing Chess, swimming, listening music, surfing Internet, self-learning through e-courses.</li> </ul>
+               </p>
+               <h3>Other Information</h3>
+               <p>
+               <ul>
+                   <li>
+                   <span id="course-name">Expected Salary:</span>As per company standards</li>
+                   <li>
+                   <span id="course-name">Area of Interest:</span>Software Development, Programming, Start-ups, Coding, App Development, Technical Support, Support Engineer, Customer Happiness, Client service, Investment Banking, Corporate Finance, Hedge Funds, Mergers & Acquisitions, Analyst, Equity Research, Business Analysis</li>
+                   <li>
+                   <span id="course-name">Joining Date:</span>Immediate</li></ul>
+               </p>
+               <h3>Declaration</h3>
+               <p>
+               I hereby declare that the details furnished above are true and correct to the best of my knowledge and belief.</p>
+        </div>
+        <div id="footer"></div>
+       </body>
+   
+    
+  );
+}
+
+export default HomePage;
